cc8d0c37f746dc927e52b70954f3fb76430b39a7
Sravanthiyadav25 <sravanthigunnala01@gmail.com>
2026-04-27 14:53:23 +0530

    first commit

--- e-display-with-react/publisher-frontend/subscriber-frontend/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Display from "./Display";
-import "./styles.css";
-
-createRoot(document.getElementById("root")).render(<Display />);
